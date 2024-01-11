2c0606ca355f5ad920bf91fdd103f0de63afdc74
uto-usui <me@uto-usui.com>
2024-01-11 11:49:42 +0900

    Add comments to filterCards function in utils
    Detailed comments have been added to the filterCards function within utils to improve code readability. These comments clarify the function's purpose, its parameters, and return value - the function is responsible for filtering an array of cards based on specified category and sub-category.

--- src/utils/filterCards.ts
+++ src/utils/filterCards.ts
@@ -1,5 +1,14 @@
 import type { Card } from '@/components/Card'
 
+/**
+ * Filters an array of cards based on the category and sub-category.
+ *
+ * @param {Card[]} cards - The array of cards to filter.
+ * @param {string | string[]} category - The category or categories to filter by.
+ * @param {string | string[]} [subCategory] - The sub-category or sub-categories to filter by (optional).
+ *
+ * @returns {Card[]} - The filtered array of cards.
+ */
 const filterCards = (
   cards: Card[],
   category: string | string[],
@@ -8,7 +17,7 @@ const filterCards = (
   return cards.filter((card) => {
     return (
       card.category === category &&
-      (!(subCategory === undefined) || card.subCategory === subCategory)
+      (typeof subCategory === 'undefined' || card.subCategory === subCategory)
     )
   })
 }
